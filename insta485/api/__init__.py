02b6e28b9e6789b51de72fd4e731f43a899cdcbd
shijurodhaz <rodneyss@umich.edu>
2019-02-12 18:06:53 -0500

    Try to fix get_multiple_posts.py

--- insta485/api/__init__.py
+++ insta485/api/__init__.py
@@ -2,6 +2,4 @@
 
 from insta485.api.likes import get_likes
 from insta485.api.url import get_url
-from insta485.api.post import get_posts, get_posts_size, \
-							  get_posts_page, \
-							  get_posts_page_size
+from insta485.api.post import get_multiple_posts
--- insta485/api/post.py
+++ insta485/api/post.py
@@ -34,50 +34,10 @@ def get_n_posts(size=10, page=0):
     return results
                                                                                    
 @insta485.app.route('/api/v1/p/', methods=["GET"])
-def get_posts():
-    """Return the 10 newest posts."""
-    print(flask.request.query_string);
-    context = {}
-    results = get_n_posts()
-    if(len(results) > 10):
-        results = results[:-1]
-    context['results'] = results
-    context['url'] = "/api/v1/p/"
-    context['next'] = "" #TODO
-    return flask.jsonify(**context)
-
-@insta485.app.route('/api/v1/p/?size=<int:size_url_slug>', methods=["GET"])
-def get_posts_size(size_url_slug):
-    """Return n newest posts."""
-    size = size_url_slug
-    context = {}
-    results = get_n_posts(size)
-    context['next'] = ""
-    if(len(results) > size):
-        results = results[:-1]
-        context["next"] = "/api/v1/p/?size=" + str(size_url_slug) + "&page=1"
-    context['url'] = "/api/v1/p/"
-    return flask.jsonify(**context)
-
-@insta485.app.route('/api/v1/p/?page=<int:page_url_slug>', methods=["GET"])
-def get_posts_page(page_url_slug):
-    """Return page n of newest posts."""
-    page = page_url_slug
-    context = {}
-    results = get_n_posts(10, page)
-    context['next'] = ""
-    if(len(results) > 10):
-        results = results[:-1]
-        page += 1
-        context["next"] = "/api/v1/p/?page=" + str(page)
-    context['url'] = "/api/v1/p/"
-    return flask.jsonify(**context)
-
-@insta485.app.route('/api/v1/p/?size=<int:size_url_slug>&page=<int:page_url_slug>', methods=["GET"])
-def get_posts_page_size(size_url_slug, page_url_slug):
-    """Return page p of newest posts with n posts per page."""
-    page = page_url_slug
-    size = size_url_slug
+def get_multiple_posts():
+    """Return the n newest posts on page p."""
+    size = flask.request.args.get("size", default=10, type=int)
+    page = flask.request.args.get("page", default=0, type=int)
     context = {}
     results = get_n_posts(size, page)
     context['next'] = ""
@@ -85,5 +45,7 @@ def get_posts_page_size(size_url_slug, page_url_slug):
         results = results[:-1]
         page += 1
         context["next"] = "/api/v1/p/?size=" + str(size) + "&page=" + str(page)
+    context['results'] = results
     context['url'] = "/api/v1/p/"
+    context['next'] = "" #CHECK
     return flask.jsonify(**context)
