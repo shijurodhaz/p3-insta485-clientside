bb229c901381c2d24fb934593d5de24117252a60
shijurodhaz <rodneyss@umich.edu>
2019-02-14 19:30:45 -0500

    Fix init.py merge conflict

--- insta485/api/__init__.py
+++ insta485/api/__init__.py
@@ -3,8 +3,5 @@
 from insta485.api.likes import get_likes
 from insta485.api.url import get_url
 from insta485.api.post import get_multiple_posts
-<<<<<<< HEAD
 from insta485.api.postid import get_post
-=======
 from insta485.api.comments import get_comments
->>>>>>> 5b5a502a9011b2ea199b72a8a36982681b2a85da
