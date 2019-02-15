3884f90ff27a1cff978c673253de9741f4f12e59
VVHack <vedagupt@umich.edu>
2019-02-14 19:42:06 -0500

    Add status codes for comments

--- insta485/api/comments.py
+++ insta485/api/comments.py
@@ -4,6 +4,7 @@ import insta485
 
 
 def handle_post(postid):
+    """Handle post requests for this endpoint"""
     data = flask.request.get_json()
     if 'text' in data:
         query = '''
@@ -12,19 +13,29 @@ def handle_post(postid):
         VALUES(?, ?, ?);
         '''
         insta485.model.query_db(query,
-                                (flask.session['username'],                        
-                                 postid,                     
+                                (flask.session['username'],
+                                 postid,
                                  data['text']))
+
 
 @insta485.app.route('/api/v1/p/<int:postid_url_slug>/comments/',
                     methods=['GET', 'POST'])
 def get_comments(postid_url_slug):
     """Return a list of comments for this post"""
+    context = {}
+    query = '''
+    SELECT * FROM posts WHERE postid=?
+    '''
+    result = insta485.model.query_db(query, (postid_url_slug,))
+    if not result:
+        context = {"message": "Not Found", "status_code": 404}
+        return flask.jsonify(**context), 404
+    status = 200
     post_request = False
     if flask.request.method == 'POST':
         post_request = True
+        status = 201
         handle_post(postid_url_slug)
-    context = {}
     context['url'] = '/api/v1/p/' + str(postid_url_slug) + '/comments/'
     query = '''
     SELECT commentid, owner, postid, text FROM comments
@@ -36,4 +47,4 @@ def get_comments(postid_url_slug):
     for comment in comments:
         comment['owner_show_url'] = '/u/' + comment['owner'] + '/'
     context['comments'] = comments
-    return flask.jsonify(**context)
+    return flask.jsonify(**context), status
