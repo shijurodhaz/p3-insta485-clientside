03826c98e7458902c46657723b4848ddaa841325
shijurodhaz <rodneyss@umich.edu>
2019-02-16 16:20:33 -0500

    Implement main.jsx and comments.jsx not tested

---
+++ insta485/js/comments.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Comments extends React.Component{
+    /* Display comments for one post
+    */
+
+    constructor(props) {
+        // Initialize mutable state
+        super(props);
+        this.state = { value: "", comments: [], items: [] };
+    }
+
+    componentDidMount() {
+        this.setState({
+            comments = [
+                {
+                    "text": "This is practice",
+                    "owner": "rodneyss",
+                    "owner_show_url": "/u/rodneyss/"
+                },
+                {
+                    "text": "This is another practice",
+                    "owner": "vedagupt",
+                    "owner_show_url": "/u/vedagupt/"
+                }
+            ]
+        });
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        logname_show_url = "/u/" + this.props.logname + "/";
+        updateComment(logname_show_url, this.props.logname, this.state.value);
+        event.preventDefault();
+    }
+
+    updateComment(owner_show_url, owner, text) {
+        this.state.items.push(
+            <a href="{owner_show_url}" style="text-decoration:none;color:black">
+                <b>{owner}</b>
+            </a>
+            {text}
+        )
+    }
+
+    render() {
+        //render comments
+
+        this.state.comments.forEach(function(comment) {
+            updateComment(comment['owner_show_url'], comment['owner'], comment['text']);
+        });
+
+        return (
+            {this.state.items}
+            <form id="comment-form" onSubmit={this.handleSubmit}>
+                <input type="text" value={this.state.value} onChange = {this.handleChange}/>
+            </form>
+        );
+    }
+}
+
+expore default Comments;
--- insta485/js/main.jsx
+++ insta485/js/main.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Likes from './likes';
 
+logname = document.getElementById('logname').innerHTML
+
 ReactDOM.render(
-  <Likes url="/api/v1/p/1/likes/" />,
+  <Post url="/api/v1/p/1/" logname = {logname}/>,
   document.getElementById('reactEntry'),
 );
